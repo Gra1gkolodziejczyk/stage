14d9bd4bd9f34e83243b3fbbcffbdd9719475331
Farixo <flavio.defreitas@ynov.com>
2022-05-05 15:33:55 +0200

    Va te faire enculer Css

--- pages/Footer/Footer.style.js
+++ pages/Footer/Footer.style.js
@@ -4,41 +4,30 @@ const FooterWrapper = styled.div`
   background-color: #3b3361;
   color: white;
   display: flex;
-  margin-top: 510px;
 `;
 
 export const ImageWrapper = styled.div`
-  padding-left: 75px;
   display: flex;
-  justify-content: flex-start;
+  align-items: center;
 `;
 
-export const TextWrapper = styled.div`
+/*export const TextWrapper = styled.div`
+  min-width: 285px;
   display: flex;
-  justify-content: center;
-  align-items: center;
   flex-direction: row;
-`;
+  justify-content: space-between;
+  align-items: center;
+`;*/
 
 export const ContentWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  padding-left: 160px;
+  justify-content: space-around;
+  flex-direction: row;
 `;
 
 export const Text = styled.h2`
-  width: 285px;
   height: 60px;
   font-size: 20px;
-  padding: 30px;
-`;
-
-export const TextC = styled.h2`
-  padding-left: 30px;
-  height: 20px;
-  font-size: 14px;
-  display: flex;
-  align-items: flex-end;
 `;
 
 export default FooterWrapper;
