139ff182a7823d725bf3cce677c899349f1205b1
Graig KOLODZIEJCZYK <graig.kolodziejczyk@ynov.com>
2022-05-10 22:00:06 +0200

    all files

--- pages/Register/Institutionnel.js
+++ pages/Register/Institutionnel.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import Link from 'next/link';
 import Footer from '../Footer/Footer';
+import Header from '../Header/Header2';
 
 import WrapperContent,{
   WrapperInscription,
@@ -13,29 +14,95 @@ import WrapperContent,{
 } from "./Institutionnel.style";
 
 const Institutionnel = () => {
+
+  const [entityName, setEntityName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [pseudo, setPseudo] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function signUp() {
+    
+    let item = {entityName, firstName, lastName, pseudo, email, password};
+    console.warn(item);
+
+    let result = await fetch(config.api_url+"/api/users/", {
+      method: "POST",
+      body:JSON.stringify(item),
+      headers : {
+        "Content-Type" : "application/json",
+        "Accept" : "application/json"
+      }
+    });
+    result = await result.json();
+    console.warn("result");
+  }
+
   return (
     <WrapperContent>
+      <Header />
       <WrapperInscription>
-        <Title>Création du compte</Title>
-        <WrapperInput>
-          <input placeholder="Creation du compte" />
-        </WrapperInput>
         <Title>Entité / Raisons sociale ou Nom Prénom</Title>
         <WrapperInput>
-          <input placeholder="Entité / Raisons sociale ou Nom Prénom" />
+          <input 
+            type="text"
+            placeholder="Entité / Raisons sociale"
+            value={entityName}
+            onChange={(e) => setEntityName(e.target.value)}
+             />
         </WrapperInput>
-        <Title>Mail</Title>
+        <Title>Nom</Title>
         <WrapperInput>
-          <input placeholder="Mail" />
+          <input 
+            type="text"
+            placeholder="Nom"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+             />
+        </WrapperInput>
+        <Title>Prénom</Title>
+        <WrapperInput>
+          <input 
+            type="text"
+            placeholder="Prénom"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+             />
+        </WrapperInput>
+        <Title>Pseudo</Title>
+        <WrapperInput>
+          <input 
+            type="text"
+            placeholder="Pseudo"
+            value={pseudo}
+            onChange={(e) => setPseudo(e.target.value)}
+             />
+        </WrapperInput>
+        <Title>Email</Title>
+        <WrapperInput>
+          <input 
+            type="text"
+            placeholder="Mail" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </WrapperInput>
         <Title>Mot de passe</Title>
         <WrapperInput>
-          <input placeholder="Mot de passe" />
+          <input 
+            type="text"
+            placeholder="Mot de passe" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
         </WrapperInput>
-        <Title>Confirmer mot de passe</Title>
-        <WrapperInput>
-          <input placeholder="Confirmer mot de passe" />
-        </WrapperInput>
+        {/*
+          <Title>Confirmer mot de passe</Title>
+          <WrapperInput>
+            <input placeholder="Confirmer mot de passe" />
+          </WrapperInput> 
+          */}
         <Subtitle>
           Vous avez déjà un compte ?
           <Link href="/Login/Demandeur_competence">
@@ -44,14 +111,13 @@ const Institutionnel = () => {
         </Subtitle>
 
         <WrapperButton>
-          {/* Wrapper coche */}
-          <Button>
-            <Link href="/OffreurDeCompetence/Conseil">
-              <a>
-                <Text>Connexion</Text>
-              </a>
-            </Link>
-          </Button>
+        <Button onClick={() => {signUp()}}>
+          <Link href="/OffreurDeCompetence/Conseil">
+          <a>
+            <Text>Connexion</Text>
+          </a>
+          </Link>
+        </Button>
         </WrapperButton>
       </WrapperInscription>
       <Footer />
---
+++ pages/api/get_api_data.js
@@ -0,0 +1,4 @@
+const config = require("../../config.json");
+const axios = require(axios);
+
+// API => FUNCTION GET REQUEST
